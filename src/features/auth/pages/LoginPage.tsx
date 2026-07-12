6501bc5fbcfa6abb13bdfd04d17a41efa82ea6c6
MUHAMMED FAYAZ T S <muhammedfayazts01@gmail.com>
2026-07-12 10:53:32 +0530

    feat: configure shacn

--- src/features/auth/pages/LoginPage.tsx
+++ src/features/auth/pages/LoginPage.tsx
@@ -1,9 +1,13 @@
 import LoginForm from '@/features/auth/components/LoginForm';
+import { Card, CardContent } from '@/shared/components/ui/card';
 
 export default function LoginPage() {
   return (
-    <div>
-      <LoginForm />
-    </div>
+    <Card>
+      <CardContent>
+        <LoginForm />
+        Hello from shadcn
+      </CardContent>
+    </Card>
   );
 }
