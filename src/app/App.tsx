446b1783879c3f94009103c242af95cb0b7660fa
MUHAMMED FAYAZ T S <muhammedfayazts01@gmail.com>
2026-07-12 09:18:06 +0530

    initialize react project

---
+++ src/app/App.tsx
@@ -0,0 +1,11 @@
+import AuthRoutes from "@routers/AuthRoutes"
+
+function App() {
+  return (
+    <div className="bg-red-500">
+      <AuthRoutes />
+    </div>
+  )
+}
+
+export default App
---
+++ src/features/login/pages/LoginPage.tsx
@@ -0,0 +1,9 @@
+import LoginForm from '@features/login/components/LoginForm';
+
+export default function LoginPage() {
+  return (
+    <div>
+      <LoginForm />
+    </div>
+  );
+}
